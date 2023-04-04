f1be6ffa39bf0765df57656461fa07c2fb380ebc
El Mery <alejandromerya@gmail.com>
2023-04-04 16:36:15 -0500

    es12

---
+++ es12/01-replaceall.js
@@ -0,0 +1,6 @@
+const string = 'javascript is a wonderful programming language';
+
+const replaceString = string.replace('javascript', 'typescript');
+
+console.log(string);
+console.log(replaceString);
